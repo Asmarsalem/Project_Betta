504874b95222096fcba5e3044d891fa1703e951e
ao20o02 <markazoussayli@gmail.com>
2023-12-28 10:29:17 +0200

    Update GASCompanionDemo.Build.cs
    add navigation system module

--- Source/GASCompanionDemo/GASCompanionDemo.Build.cs
+++ Source/GASCompanionDemo/GASCompanionDemo.Build.cs
@@ -8,7 +8,7 @@ public class GASCompanionDemo : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore","GASCompanion","TestGFRuntime","PlayFab","HTTP"});
+		PublicDependencyModuleNames.AddRange(new string[] {"NavigationSystem", "Core", "CoreUObject", "Engine", "InputCore","GASCompanion","TestGFRuntime","PlayFab","HTTP"});
 
 		PrivateDependencyModuleNames.AddRange(new string[]
 		{
