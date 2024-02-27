41a8c2bb86db57094f9aed22b78f52f1aafc3c08
ao20o02 <124131550+ao20o02@users.noreply.github.com>
2024-02-27 10:57:25 +0200

    ricochet arrow

---
+++ Source/GASCompanionDemo/Private/Actor/ABP_Recohet_Arrorw.cpp
@@ -0,0 +1,71 @@
+﻿// Copyright 2021-2022 Mickael Daniel. All Rights Reserved.
+
+
+#include "Actor/ABP_Recohet_Arrorw.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemInterface.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
+
+
+// Sets default values
+AABP_Recohet_Arrorw::AABP_Recohet_Arrorw()
+{
+	// Init Root Component
+	DefaultSceneRoot=CreateDefaultSubobject<USceneComponent>("Root");
+	RootComponent = DefaultSceneRoot;
+	// init Static Mesh
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	StaticMesh->SetupAttachment(RootComponent);
+	// init Box Component
+	BoxComponent=CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComponent"));
+	BoxComponent->SetupAttachment(StaticMesh);
+	// Init Projectile
+	ProjectileMovementComponent=CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovementComponent"));
+}
+
+// Called when the game starts or when spawned
+void AABP_Recohet_Arrorw::BeginPlay()
+{
+	Super::BeginPlay();
+	BoxComponent->OnComponentBeginOverlap.AddDynamic(this,&AABP_Recohet_Arrorw::OnComponentBeginOvelarp);
+	
+}
+
+void AABP_Recohet_Arrorw::OnComponentBeginOvelarp(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	/*if( OtherActor && OtherActor != GetInstigator())
+	{Bouncing();
+		if(IAbilitySystemInterface* AbilitySystemInterface=Cast<IAbilitySystemInterface>(OtherActor->GetInstigator()))
+		{
+			UAbilitySystemComponent* AbilitySystemComponent=Cast<UAbilitySystemComponent>(AbilitySystemInterface->GetAbilitySystemComponent());
+			FGameplayEffectSpecHandle EffectSpecHandle = AbilitySystemComponent->MakeOutgoingSpec(GameplayEffect, 0.f, AbilitySystemComponent->MakeEffectContext());
+			if(EffectSpecHandle.IsValid())
+			{
+				UE_LOG(LogTemp,Error,TEXT("ApplyGameplayEffectSpecToTarget"));
+				AbilitySystemComponent->ApplyGameplayEffectSpecToTarget(*EffectSpecHandle.Data.Get(),AbilitySystemComponent);
+			}
+		}
+	}*/
+}
+
+void AABP_Recohet_Arrorw::Bouncing()
+{
+	if(CurrentIndex>MaxIndex)
+	{
+		K2_DestroyActor();
+	}
+	CurrentIndex++;
+	TArray<AActor*> OutActors;
+	float ClosetActorDistance;
+	UGameplayStatics::GetAllActorsOfClass(this,AiParent,OutActors);
+	AActor* ClosetActor=UGameplayStatics::FindNearestActor(FVector::Zero(),OutActors,ClosetActorDistance);
+	FRotator ActorRotation=UKismetMathLibrary::FindLookAtRotation(GetActorLocation(),ClosetActor->GetActorLocation());
+	FVector VelocityCalculation=UKismetMathLibrary::GetForwardVector(ActorRotation)*ProjectileVelocitySpeed;
+	ProjectileMovementComponent->Velocity=VelocityCalculation;
+}
+
+
+
---
+++ Source/GASCompanionDemo/Public/Actor/ABP_Recohet_Arrorw.h
@@ -0,0 +1,56 @@
+﻿// Copyright 2021-2022 Mickael Daniel. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "Components/BoxComponent.h"
+#include "GameFramework/Actor.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "ABP_Recohet_Arrorw.generated.h"
+
+UCLASS()
+class GASCOMPANIONDEMO_API AABP_Recohet_Arrorw : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AABP_Recohet_Arrorw();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Define Components
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category="Components | Viewport")
+	USceneComponent* DefaultSceneRoot;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category="Components | Viewport")
+	UStaticMeshComponent* StaticMesh;
+	UPROPERTY(VisibleAnywhere,BlueprintReadWrite, Category="Components | Viewport")
+	UBoxComponent* BoxComponent;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category="Components | Viewport")
+	UProjectileMovementComponent* ProjectileMovementComponent;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category="Components | Config")
+	TSubclassOf<AActor> AiParent;
+
+	
+	// Init Vars
+	UPROPERTY(EditDefaultsOnly,BlueprintReadWrite, Category="Components | ProjectileMovementComponent")
+	int MaxIndex;
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly, Category="Components | ProjectileMovementComponent")
+	float ProjectileVelocitySpeed;
+	UPROPERTY()
+	int CurrentIndex;
+
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly, Category="GAS | GameplayEffect")
+	TSubclassOf<UGameplayEffect> GameplayEffect;
+
+	//Default Func
+	UFUNCTION()
+	void OnComponentBeginOvelarp(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+	UFUNCTION(BlueprintCallable, Category="Components | ProjectileMovementComponent | Func")
+	void Bouncing();
+	
+};
